2c09ee5d3e56ffba9df98e3de138deb4df43ca9b
spacew010-2000 <spacew010@gmail.com>
2026-04-17 09:55:35 +0500

    last-finished

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+
+// Variables
+let bought = 9
+console.log(bought);
+
+var sell = "sotish"
+console.log(sell);
+
+const book = "kitob"
+console.log(book);
+
+// Array
+
+let arr = [2,3,4,5,"olma","kitob"]
+console.log(arr);
+
+arr.push("soat")
+console.log(arr);
+
+// Function
+
+
+
+
+
+
+
